0dd4283c82d70c2a9bf318ac25ac930c2d708093
LFXIII <33501377+LFXIII@users.noreply.github.com>
2017-11-10 22:20:48 -0500

    Add files via upload
    Hey. This is my first commit change. 
    
    In here you'll find a more complete class diagram as well as generated code for our project. It is ready to be coded.

---
+++ shpcart-master/ShoppingCart/src/coe528/project/Item.java
@@ -0,0 +1,20 @@
+package coe528.project;
+
+public class Item {
+
+    private String name;
+
+    private String condition;
+
+    private int productID;
+
+    private double price;
+
+    public Item() {
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
