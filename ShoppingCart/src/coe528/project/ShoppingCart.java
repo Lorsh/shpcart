710cb56b5cfe1fbf2e5b8e5672d24add8717c82f
Subash <subhasish.roy@ryerson.ca>
2017-11-17 21:30:23 -0500

    UML changes in ShoppingCart and the relation betw customer

--- ShoppingCart/src/coe528/project/ShoppingCart.java
+++ ShoppingCart/src/coe528/project/ShoppingCart.java
@@ -1,5 +1,6 @@
 package coe528.project;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ShoppingCart {
@@ -10,18 +11,64 @@ public class ShoppingCart {
 
     private double subtotal;
 
-    public ShoppingCart() {
+    public ShoppingCart(int shoppingID, double subtotal) {
+        this.items = new ArrayList<Item>(); 
+        this.shoppingID = shoppingID;
+        this.subtotal = subtotal;
+        
+        
+    }
+    public int getshoppingID(){
+        return shoppingID; // getting the ID of the item
+    }
+    public void addtoShoppingCart(Item item){
+        items.add(item);
+    }
+    public double getsubtotal(){
+    double x = 0;
+//summation of all the item
+    for(int i=0;i<items.size() ;i++){
+        x = items.get(shoppingID).getPrice();
+        subtotal = subtotal + x;
+           
+    }
+        return (subtotal);
     }
 
     public void checkOut() {
+       Order y = new Order();
+        
     }
 
-    public void updateQuantity() {
+    public void updateQuantity(Catalog n) { //take the reference of the item from the shopping chart and put that into the chopping chart
+        // find reference of item in the catalog. If they matches 
+        int currentId;
+        for(int i=0;i<items.size(); i++){
+        currentId = items.get(i).getProductID();
+        try{
+        n.removeItemFromCatalog(currentId);
+        }
+        catch(IOException e)
+                {
+                    System.out.println("Invalid ID of the Item");
+                }
+        }
+    }
+    
+    public int TotalShoppingNumber(){
+        return (items.size());
     }
 
     public void viewCartDetails() {
+        //int shoppingTotal = items.size();
+        System.out.println("Shopping ID is " +getshoppingID() +"\nNumber of Items " +TotalShoppingNumber()+ "\nSubtotal " +getsubtotal() );
     }
-
-    public void calculateSub() {
+// adding TAX (HST) to the SubTotal
+    public double calculateSub() {
+        double m = ((0.13*(subtotal))+subtotal);
+        return m;
+    }
+    public String toString() {
+        return "Numbers of Items" +TotalShoppingNumber()+ "Total " +getsubtotal() +"After Tax " +calculateSub();
     }
 }
