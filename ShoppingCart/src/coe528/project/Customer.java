b35d09caf4d838eaabf1a23b20d25fa1a1f7fd36
lfxiii <djoe816@gmail.com>
2017-11-21 00:03:22 -0500

    Adding everything again cause im an idiot

--- ShoppingCart/src/coe528/project/Customer.java
+++ ShoppingCart/src/coe528/project/Customer.java
@@ -1,5 +1,7 @@
 package coe528.project;
 
+import java.util.Scanner;
+
 public class Customer {
 
     private String name;
@@ -12,15 +14,65 @@ public class Customer {
 
     private ShippingInfo shippingInfo;
 
-    private ShoppingCart shpCart;
-            
     private double accountBalance;
 
     private Order order;
+    
+    private ShoppingCart shoppingCart;
 
     public Customer() {
     }
-
+    
+    
+    // User seen products
+    // User choosing products by this stage
+    public void addToCart(){
+        int iD;
+        int amount;
+        int i;
+        String iName;
+        String iCond = null;
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Please enter the name of the item you want to add the shopping cart: ");
+        iName = s.next();
+        System.out.println("How many would you like have: ");
+        while(!s.hasNextInt()){
+            s.next();
+        }
+        amount = s.nextInt();
+        
+        if(Catalog.getInstance().getItem(iName, iCond) != null){
+            shoppingCart.updateQuantity(amount, shoppingCart.getItem(iName, iCond));
+        }
+        
+        System.out.println("Added " + amount + " " + iName + " to shopping cart");
+    }
+    
+        public void removeToCart(){
+        int iD;
+        int amount;
+        int i;
+        String iName;
+        String iCond = null;
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Please enter the name of the item you want to remove the shopping cart: ");
+        iName = s.next();
+        System.out.println("How many would you like have: ");
+        while(!s.hasNextInt()){
+            s.next();
+        }
+        amount = s.nextInt();
+        
+        if(Catalog.getInstance().getItem(iName, iCond) != null){
+            shoppingCart.updateQuantity(amount, shoppingCart.getItem(iName, iCond));
+        }
+        
+        System.out.println("Removed " + amount + " " + iName + " to shopping cart");
+    }
+    
     public void login() {
+        
     }
 }
